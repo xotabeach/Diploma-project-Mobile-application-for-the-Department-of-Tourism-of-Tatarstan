ae809edd51f4daedd70c58a284437b1f938e6310
Nick Tarasov <xotabich1941@gmail.com>
2023-12-02 23:24:52 +0300

    fix some bugs and finish nav_bar widget + resize on States main activity

--- app/src/main/java/com/example/mobile_tour/MainActivity.java
+++ app/src/main/java/com/example/mobile_tour/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.mobile_tour;
 
 import android.os.Bundle;
+import android.os.Build;
 
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
+import com.example.mobile_tour.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,7 +23,19 @@ import com.example.mobile_tour.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private ActivityMainBinding binding;
+
+
+    protected void hideSystemBars(){
+        Window window = getWindow();
+        window.setDecorFitsSystemWindows(false);
+        WindowInsetsController controller = window.getInsetsController();
+        if (controller != null) {
+            controller.hide( WindowInsets.Type.navigationBars());
+            controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);}
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        //
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -32,6 +54,25 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        hideSystemBars();
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        hideSystemBars();
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        hideSystemBars();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        hideSystemBars();
+    }
 }
--- app/src/main/java/com/example/mobile_tour/ui/right_bar/RightBarFragment.java
+++ app/src/main/java/com/example/mobile_tour/ui/right_bar/RightBarFragment.java
@@ -1,4 +1,38 @@
 package com.example.mobile_tour.ui.right_bar;
 
-public class RightBarFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mobile_tour.databinding.FragmentRightBarBinding;
+import com.example.mobile_tour.ui.right_bar.RightBarViewModel;
+
+public class RightBarFragment extends Fragment {
+
+    private FragmentRightBarBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        RightBarViewModel rightBarViewModel =
+                new ViewModelProvider(this).get(RightBarViewModel.class);
+
+        binding = FragmentRightBarBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        rightBarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
