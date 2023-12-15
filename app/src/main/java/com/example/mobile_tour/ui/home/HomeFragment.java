cd3b5dc998e8fa2167bf735286395bd8f8d6412e
Nick Tarasov <xotabich1941@gmail.com>
2023-12-15 20:00:34 +0300

    add new kt file to carousel view

--- app/src/main/java/com/example/mobile_tour/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mobile_tour/ui/home/HomeFragment.java
@@ -4,40 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.example.mobile_tour.R;
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.motion.widget.MotionScene;
+import androidx.compose.ui.platform.ComposeView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.mobile_tour.databinding.FragmentHomeBinding;
-
-
 
 public class HomeFragment extends Fragment {
 
-
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
+    CarouselMainKt carouselMainKt = new CarouselMainKt();
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ComposeView composeView = root.findViewById(R.id.composeView);
+       carouselMainKt.CarouselCard(composeView);
+        return root; // Используйте root вместо composeView
     }
 }
